3231ab5f70f21f390d7073af8247bd08b6da7e28
Patty R. <smyleeface@users.noreply.github.com>
2024-06-23 23:10:28 -0700

    feat: add lambda function blog_post_image_generator

---
+++ functions/blog_post_image_generator/hugo_site/blog_post_image_generator/bedrock_images.py
@@ -0,0 +1,90 @@
+import base64
+import json
+import os
+
+
+def invoke_model(bedrock_client, input_text, model_name):
+    try:
+        bedrock_response = bedrock_client.invoke_model(
+            body=json.dumps(input_text).encode("utf-8"),
+            contentType="application/json",
+            accept="application/json",
+            modelId=model_name,
+        )
+        response_body = json.loads(bedrock_response["body"].read().decode("utf-8"))
+        return response_body
+    except Exception as e:
+        print(e)
+        raise e
+
+
+def save_image(image_data, idx, image_temp_directory, filename):
+    base_filename = os.path.basename(filename).replace(".md", "")
+    base_pathname = os.path.dirname(filename).replace("content/post/", "images/")
+    os.makedirs(f"{image_temp_directory}/{base_pathname}", exist_ok=True)
+    image_filename = f"{base_pathname}/{base_filename}_image_{idx}.png"
+    with open(f"{image_temp_directory}/{image_filename}", "wb") as image_file:
+        image_file.write(base64.b64decode(image_data))
+    return image_filename
+
+
+def run_stability_ai(
+    prompt,
+    negative_prompt,
+    bedrock_client,
+    filename,
+    image_temp_directory,
+    number_of_images=1,
+):
+    input_text = {
+        "text_prompts": [
+            {"text": prompt, "weight": 1, "negativeText": negative_prompt}
+        ],
+        "cfgScale": 8,
+        "seed": 0,
+        "steps": 50,
+        "quality": "standard",
+        "width": 512,
+        "height": 512,
+        "numberOfImages": number_of_images,
+    }
+    model_name = "stability.stable-diffusion-xl-v1"
+    response_body = invoke_model(bedrock_client, input_text, model_name)
+    images = response_body["artifacts"]
+    # Save images locally
+    filenames = []
+    for idx, image_data in enumerate(images):
+        filenames.append(
+            save_image(image_data.get("base64"), idx, image_temp_directory, filename)
+        )
+    return filenames
+
+
+def run_titan_image_generator(
+    prompt,
+    negative_prompt,
+    bedrock_client,
+    filename,
+    image_temp_directory,
+    number_of_images=1,
+):
+    input_text = {
+        "textToImageParams": {"text": prompt, "negativeText": negative_prompt},
+        "taskType": "TEXT_IMAGE",
+        "imageGenerationConfig": {
+            "cfgScale": 8,
+            "seed": 0,
+            "quality": "standard",
+            "width": 1024,
+            "height": 1024,
+            "numberOfImages": number_of_images,
+        },
+    }
+    model_name = "amazon.titan-image-generator-v1"
+    response_body = invoke_model(bedrock_client, input_text, model_name)
+    images = response_body["images"]
+    # Save images locally
+    filenames = []
+    for idx, image_data in enumerate(images):
+        filenames.append(save_image(image_data, idx, image_temp_directory, filename))
+    return filenames
---
+++ functions/blog_post_image_generator/hugo_site/blog_post_image_generator/upload_to_s3.py
@@ -0,0 +1,11 @@
+import os
+
+
+def run(image_temp_directory, images_bucket_name, s3_client, image_info):
+    local_path = os.path.join(image_temp_directory, image_info)
+    s3_client.upload_file(
+        local_path,
+        images_bucket_name,
+        image_info,
+        ExtraArgs={"ContentType": "image/png"},
+    )
