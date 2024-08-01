a7b1078dfdc35760b110c6c64e773d1591e2acc3
Patty R. <smyleeface@users.noreply.github.com>
2024-08-01 13:54:33 -0700

    feat: supports multiple images and better filename format

--- functions/blog_post_image_generator/hugo_site/blog_post_image_generator/aws/bedrock.py
+++ functions/blog_post_image_generator/hugo_site/blog_post_image_generator/aws/bedrock.py
@@ -1,20 +1,21 @@
 import base64
+from datetime import datetime
 import json
 import os
 
 
-def generate_image(title, description, filename, bedrock_client, image_temp_directory, model_type):
-    prompt = f'Use technology and scenic related imagery where the title and description are:\n\n"{title}"\n"{description}"\n\n.'
+def generate_image(title, description, filename, bedrock_client, image_temp_directory, model_type, number_of_images=1):
+    prompt = f'Use a combination of technology, scenic, and animal imagery where the title and description are:\n\n"{title}"\n"{description}"\n\n.'
     negative_prompt = "avoid using typography or text"
     if model_type == "stability_ai":
         bedrock_image_names = run_stability_ai(
-            prompt, negative_prompt, bedrock_client, filename, image_temp_directory
+            prompt, negative_prompt, bedrock_client, filename, image_temp_directory, number_of_images
         )
     else:
         bedrock_image_names = run_titan_image_generator(
-            prompt, negative_prompt, bedrock_client, filename, image_temp_directory
+            prompt, negative_prompt, bedrock_client, filename, image_temp_directory, number_of_images
         )
-    return bedrock_image_names[0]
+    return bedrock_image_names
 
 
 def invoke_model(bedrock_client, input_text, model_name):
@@ -32,12 +33,11 @@ def invoke_model(bedrock_client, input_text, model_name):
         raise e
 
 
-def save_image(image_data, idx, image_temp_directory, filename):
+def save_image(image_data, idx, image_temp_directory, filename, timestamp):
     base_filename = os.path.basename(filename).replace(".md", "")
     base_pathname = os.path.dirname(filename).replace("content/post/", "images/")
     os.makedirs(f"{image_temp_directory}/{base_pathname}", exist_ok=True)
-    # TODO: Use a better image filename format
-    image_filename = f"{base_pathname}/{base_filename}_image_{idx}.png"
+    image_filename = f"{base_pathname}/{base_filename}_image_{idx}_{timestamp}.png"
     with open(f"{image_temp_directory}/{image_filename}", "wb") as image_file:
         image_file.write(base64.b64decode(image_data))
     return image_filename
@@ -68,9 +68,10 @@ def run_stability_ai(
     images = response_body["artifacts"]
     # Save images locally
     filenames = []
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     for idx, image_data in enumerate(images):
         filenames.append(
-            save_image(image_data.get("base64"), idx, image_temp_directory, filename)
+            save_image(image_data.get("base64"), idx, image_temp_directory, filename, timestamp)
         )
     return filenames
 
@@ -98,8 +99,9 @@ def run_titan_image_generator(
     model_name = "amazon.titan-image-generator-v1"
     response_body = invoke_model(bedrock_client, input_text, model_name)
     images = response_body["images"]
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     # Save images locally
     filenames = []
     for idx, image_data in enumerate(images):
-        filenames.append(save_image(image_data, idx, image_temp_directory, filename))
+        filenames.append(save_image(image_data, idx, image_temp_directory, filename, timestamp))
     return filenames
