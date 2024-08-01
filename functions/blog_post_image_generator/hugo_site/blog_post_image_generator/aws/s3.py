ded16cda487760d5f2f877cae55dfe82f17dc3a3
Patty R. <smyleeface@users.noreply.github.com>
2024-08-01 13:54:49 -0700

    feat: supports custom content types

--- functions/blog_post_image_generator/hugo_site/blog_post_image_generator/aws/s3.py
+++ functions/blog_post_image_generator/hugo_site/blog_post_image_generator/aws/s3.py
@@ -1,11 +1,11 @@
 import os
 
 
-def upload(image_temp_directory, images_bucket_name, s3_client, image_info):
+def upload(image_temp_directory, images_bucket_name, s3_client, image_info, content_type):
     local_path = os.path.join(image_temp_directory, image_info)
     s3_client.upload_file(
         local_path,
         images_bucket_name,
         image_info,
-        ExtraArgs={"ContentType": "image/png"},
+        ExtraArgs={"ContentType": content_type},
     )
